98a5e17ec59fd3f8fcc68c875d10751783ca515a
Toni <tposcic@riteh.hr>
2018-12-06 19:29:26 +0100

    add middlewares

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -15,15 +15,15 @@ class Cors
      */
     public function handle($request, Closure $next)
     {
-        // $allowedOrigins = ['http://localhost:4200'];//localhost hack
-        // $origin = isset($_SERVER['HTTP_ORIGIN']) ? $_SERVER['HTTP_ORIGIN'] : '';
-        // if (in_array($origin, $allowedOrigins)) {
-        //     return $next($request)
-        //         ->header('Access-Control-Allow-Origin', $origin)
-        //         ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-        //         ->header('Access-Control-Allow-Headers',' Origin, Content-Type, Accept, Authorization, X-Request-With, cache-control,postman-token, token')
-        //         ->header('Access-Control-Allow-Credentials',' true');
-        // }
+        $allowedOrigins = ['http://localhost:4200'];//localhost hack
+        $origin = isset($_SERVER['HTTP_ORIGIN']) ? $_SERVER['HTTP_ORIGIN'] : '';
+        if (in_array($origin, $allowedOrigins)) {
+            return $next($request)
+                ->header('Access-Control-Allow-Origin', $origin)
+                ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+                ->header('Access-Control-Allow-Headers',' Origin, Content-Type, Accept, Authorization, X-Request-With, cache-control,postman-token, token')
+                ->header('Access-Control-Allow-Credentials',' true');
+        }
         return $next($request);
     }
 }
