ee50e100fe9ec19909b5989702cf538ce4a95cef
tposcic <tposcic@gmail.com>
2019-08-26 23:22:28 +0200

    remove origin check

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -15,15 +15,16 @@ class Cors
      */
     public function handle($request, Closure $next)
     {
-        $allowedOrigins = ['http://localhost:4200'];
-        $origin = isset($_SERVER['HTTP_ORIGIN']) ? $_SERVER['HTTP_ORIGIN'] : '';
-        if (in_array($origin, $allowedOrigins)) {
-            return $next($request)
-                ->header('Access-Control-Allow-Origin', $origin)
-                ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-                ->header('Access-Control-Allow-Headers',' Origin, Content-Type, Accept, Authorization, X-Request-With, cache-control,postman-token, token')
-                ->header('Access-Control-Allow-Credentials',' true');
-        }
-        return $next($request);
+        // $allowedOrigins = ['http://localhost:8080'];
+        // $origin = isset($_SERVER['HTTP_ORIGIN']) ? $_SERVER['HTTP_ORIGIN'] : '';
+        $origin = '*';
+        // if (in_array($origin, $allowedOrigins)) {
+        return $next($request)
+            ->header('Access-Control-Allow-Origin', $origin)
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+            ->header('Access-Control-Allow-Headers',' Origin, Content-Type, Accept, Authorization, X-Request-With, cache-control,postman-token, token')
+            ->header('Access-Control-Allow-Credentials',' true');
+        // }
+        // return $next($request);
     }
 }
